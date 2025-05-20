a967faa6165ee683f1544204aeba86c98febb0fd
David Avedis Injarabian <david@DESKTOP-T2PRON0.>
2025-05-20 11:07:30 +0000

    Initial Spring Boot setup

---
+++ src/main/java/com/example/lhv_homework/LhvHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.lhv_homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LhvHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LhvHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lhv_homework/LhvHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lhv_homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LhvHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
